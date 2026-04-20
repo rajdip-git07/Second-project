a77f39df2b7e7b8348e0ce501b01635388a1a029
Rajdip Dey <rajdipd415@gmail.com>
2026-04-20 20:03:59 +0530

    Remove commented code and clean up formatting

--- guess_number.c
+++ guess_number.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h> // for rand() and srand()
-#include <time.h>   // for time()
+#include <stdlib.h>
+#include <time.h>   
 
 int main()
 {
-    // Initialize the random number generator with the current time
+    
     srand(time(0));
 
-    // Generate a random number between 1 and 100
+  
     int randomNumber = (rand() % 100) + 1;
     int no_of_guesses = 0;
     int guessed_number;
-    // Print the random number
-    //  printf("Random number between 1 and 100: %d\n", randomNumber);
-    // printf("Guess the number");
+    
     do
     {
         printf("Guess the number");
